f9b0f8258203de9b3312deff5709ba93b352c945
wuld666 <43483384+wuld666@users.noreply.github.com>
2024-10-04 11:14:09 -0500

    Delete custom.js

--- custom.js
+++
@@ -1,7 +0,0 @@
-// custom.js
-// Hide the download button in Jupyter Notebook
-require(["base/js/namespace", "base/js/events"], function(Jupyter, events) {
-    events.on("app_initialized.NotebookApp", function() {
-        Jupyter.toolbar.actions.get("jupyter-notebook:download-notebook").remove();
-    });
-});
